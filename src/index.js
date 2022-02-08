1ba6b38d810533dcf709bcfd0b2922cf433f2bfc
taplinseth <taplinseth@gmail.com>
2022-02-08 10:13:09 -0600

    gitignore updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-// import axios here
+require('dotenv').config();
+const axios = require('axios');
+
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const API_KEY = process.env.API_KEY;
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
